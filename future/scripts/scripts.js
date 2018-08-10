1a9c803ede596195aa8c8c401a701d4ef6e1741f
akshathjain <akshath.r.jain@gmail.com>
2018-08-10 12:48:07 -0400

    updated the future countdown date

--- future/scripts/scripts.js
+++ future/scripts/scripts.js
@@ -69,7 +69,7 @@ $(document).ready(function(){
 
     //initialize the countdown clock
     function initCountdown(){
-        var deadline = "2018-09-01T03:14:15.927-05:00" //september 1, 2018 at 3:14:15.927 am est
+        var deadline = "2019-01-05T03:14:15.927-05:00" //september 1, 2018 at 3:14:15.927 am est
 
         //update clock every second
         setInterval(function(){
